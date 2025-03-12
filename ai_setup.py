8b919075ee281c568d93a0e1b7de499ec24ac504
DiyaBhujbal <diyab0225@gmail.com>
2025-03-12 17:08:36 +0530

    Preview Json

--- ai_setup.py
+++ ai_setup.py
@@ -8,8 +8,8 @@ api_key = os.getenv("GOOGLE_API_KEY")
 
 if not api_key:
     raise ValueError("API key is missing. Please set the GEMINI_API_KEY.")
-
-genai.configure(api_key=api_key)
+else:
+    genai.configure(api_key=api_key)
 
 def process_observation(observation):
     try:
